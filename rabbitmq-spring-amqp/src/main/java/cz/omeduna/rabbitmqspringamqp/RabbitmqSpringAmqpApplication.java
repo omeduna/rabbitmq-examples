91e372173319bd757ef8ec2292ff11b186b2113e
Ondrej Meduna <ondrej.meduna@embedit.cz>
2019-01-31 18:04:23 +0100

    Initial commit

---
+++ rabbitmq-spring-amqp/src/main/java/cz/omeduna/rabbitmqspringamqp/RabbitmqSpringAmqpApplication.java
@@ -0,0 +1,14 @@
+package cz.omeduna.rabbitmqspringamqp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqSpringAmqpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqSpringAmqpApplication.class, args);
+    }
+
+}
+
