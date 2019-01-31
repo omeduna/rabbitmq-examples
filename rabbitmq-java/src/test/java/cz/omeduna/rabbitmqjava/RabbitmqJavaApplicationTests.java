5e0c60b5691c0f42c8cf031fd107755d55ee44ed
Ondrej Meduna <ondrej.meduna@embedit.cz>
2019-01-31 19:36:29 +0100

    RabbitMQ Java examples

--- rabbitmq-java/src/test/java/cz/omeduna/rabbitmqjava/RabbitmqJavaApplicationTests.java
+++ rabbitmq-java/src/test/java/cz/omeduna/rabbitmqjava/RabbitmqJavaApplicationTests.java
@@ -1,16 +1,58 @@
 package cz.omeduna.rabbitmqjava;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.GetResponse;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 public class RabbitmqJavaApplicationTests {
 
+    private static final String QUEUE_NAME = "cmsg.test.queue";
+    private static final String MESSAGE = "Hello Rabbit!!!";
+
     @Test
-    public void contextLoads() {
+    public void produceMessagesTest() throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+
+        try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
+
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+            for (int i = 0; i < 3; i++) {
+                channel.basicPublish("", QUEUE_NAME, null, MESSAGE.getBytes());
+            }
+
+            System.out.println("\n [x] Sent '" + MESSAGE + "'");
+        }
+    }
+
+    @Test
+    public void consumeMessagesTest() throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+
+        try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
+
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+            while (true) {
+                GetResponse response = channel.basicGet(QUEUE_NAME, true);
+                if (response == null) {
+                    System.out.println("\nNO MORE MESSAGES!!!");
+                    break;
+                }
+                System.out.println("\nResponse: " + response);
+            }
+            System.out.println("\n");
+        }
     }
 
 }
